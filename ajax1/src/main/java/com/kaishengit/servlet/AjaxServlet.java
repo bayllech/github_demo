7aba284a15e9c732f4f1b804f9bde0d9e5fb7ace
bayllech <hpuqibei@163.com>
2016-12-05 14:48:36 +0800

    重新建立ajax1项目

---
+++ ajax1/src/main/java/com/kaishengit/servlet/AjaxServlet.java
@@ -0,0 +1,17 @@
+package com.kaishengit.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by 帅比 on 2016/12/5.
+ */
+public class AjaxServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
