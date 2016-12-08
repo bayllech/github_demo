1379a6677f1a2103561add2e5da6b44260255d88
bayllech <hpuqibei@163.com>
2016-12-08 22:50:20 +0800

    /add优化

--- web_guestbook/src/main/java/com/kaishengit/servlet/AddMessageServlet.java
+++ web_guestbook/src/main/java/com/kaishengit/servlet/AddMessageServlet.java
@@ -12,9 +12,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-/**
- * Created by 帅比 on 2016/11/23.
- */
 @WebServlet("/add")
 public class AddMessageServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
