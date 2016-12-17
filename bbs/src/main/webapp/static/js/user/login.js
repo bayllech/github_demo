48cea330088b838e9e13457e522b5764cabecb68
bayllech <hpuqibei@163.com>
2016-12-17 10:49:01 +0800

    登录进入主页完成

--- bbs/src/main/webapp/static/js/user/login.js
+++ bbs/src/main/webapp/static/js/user/login.js
@@ -3,6 +3,53 @@
  */
 $(function () {
     $("#loginbtn").click(function () {
+        $("#formLogin").submit();
+    });
+
+    $("#formLogin").validate({
+        errorElement:'span',
+        errorClass:'text-error',
+        rules:{
+            username:{
+                required:true
+            },
+            password:{
+                required:true
+            }
+        },
+        messages:{
+            username:{
+                required:"请输入登录账号"
+            },
+            password:{
+                required:"请输入密码"
+            }
+        },
+        submitHandler:function () {
+            $.ajax({
+                url: "/login",
+                type: "post",
+                data: $(form).serialize(),
+                beforeSend: function () {
+                    $("#loginbtn").text("登录中...").attr("disabled", "disabled");
+                },
+                success: function (data) {
+                    if (date.state == "success") {
+                        alert("登录成功！");
+                        window.location.href="/home";
+                    } else {
+                        alert(data.messages);
+                    }
+                },
+                error: function () {
+                    alert("服务器错误");
+                },
+                complete: function () {
+                    $("#loginbtn").text("登录").removeAttr("disabled");
+                }
+            });
+        }
+
 
     });
 });
