545dc14ac842d8382f62a0897ca5014cfda6c497
bayllech <hpuqibei@163.com>
2017-01-07 10:50:03 +0800

    创建spring项目

---
+++ spring/src/main/java/com/kaishengit/dao/Impl/UserDaoImpl.java
@@ -0,0 +1,8 @@
+package com.kaishengit.dao.Impl;
+
+/**
+ * Created by bayllech on 2017/1/7.
+ */
+public class UserDaoImpl {
+
+}
---
+++ spring/src/main/java/com/kaishengit/dao/UserDao.java
@@ -0,0 +1,6 @@
+package com.kaishengit.dao;
+
+public interface UserDao {
+    public void save();
+    public void update();
+}
