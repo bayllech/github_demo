2b210e93aae3adf362eb26f313c614e08966d25f
bayllech <hpuqibei@163.com>
2017-02-24 21:03:17 +0800

    微信AccessToken放入缓存

--- ssm_device/src/main/java/com/kaishengit/service/WXService.java
+++ ssm_device/src/main/java/com/kaishengit/service/WXService.java
@@ -1,20 +1,66 @@
 package com.kaishengit.service;
 
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+import com.google.gson.Gson;
 import com.kaishengit.exception.ServiceException.ServiceException;
 import com.qq.wx.mp.aes.AesException;
 import com.qq.wx.mp.aes.WXBizMsgCrypt;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class WXService {
 
+    Logger logger = LoggerFactory.getLogger(WXService.class);
+
+    private static final String ACCESS_TOKEN_URL = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid={0}&corpsecret={1}";
+    private static final String CREATE_USER_URL = "https://qyapi.weixin.qq.com/cgi-bin/user/create?access_token={0}";
     @Value("${wx.token}")
     private String token;
     @Value("${wx.EncodingAESKey}")
     private String aesKey;
     @Value("${wx.corpid}")
     private String corpid;
+    @Value("${wx.secret}")
+    private String secret;
+
+    private LoadingCache<String,String> cache = CacheBuilder.newBuilder()
+            .maximumSize(5)
+            .expireAfterWrite(7200, TimeUnit.SECONDS)
+            .build(new CacheLoader<String, String>() {
+                @Override
+                public String load(String s) throws Exception {
+                    String accessTokenUrl = MessageFormat.format(ACCESS_TOKEN_URL, corpid, secret);
+
+                    OkHttpClient httpClient = new OkHttpClient();
+                    Request request = new Request.Builder().url(accessTokenUrl).build();
+                    Response response = httpClient.newCall(request).execute();
+                    String result = response.body().string();
+                    response.close();
+
+                    Map<String, Objects> map = new Gson().fromJson(result, HashMap.class);
+                    if(map.containsKey("errcode")) {
+                        logger.error("获取微信AccessToken异常:{}", map.get("errcode"));
+                        throw new ServiceException("获取AccessToken异常:" + map.get("errcode"));
+                    } else {
+                        return map.get("access_token").toString();
+                    }
+                }
+            });
 
     /**
      * 微信企业号初始化
@@ -32,6 +78,22 @@ public class WXService {
         } catch (AesException e) {
             throw new ServiceException("微信初始化异常", e);
         }
+    }
+
+    /**
+     * 获取AccessToken
+     * @return
+     */
+    public String getAccessToken() {
+        try {
+            return cache.get("");
+        } catch (ExecutionException e) {
+            throw new ServiceException("获取AccessToken异常", e);
+        }
+    }
+
+    public String saveUser() {
+        
     }
 
 }
