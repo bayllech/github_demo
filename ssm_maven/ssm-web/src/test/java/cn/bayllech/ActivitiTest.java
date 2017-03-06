fcf5fe87413054e1ff3485988ffe5ef830f44f0f
bayllech <hpuqibei@163.com>
2017-03-06 17:29:11 +0800

    activiti的maven依赖

---
+++ ssm_maven/ssm-web/src/test/java/cn/bayllech/ActivitiTest.java
@@ -0,0 +1,12 @@
+package cn.bayllech;
+
+import org.activiti.engine.ProcessEngineConfiguration;
+
+public class ActivitiTest {
+
+    ProcessEngineConfiguration processEngineConfiguration = ProcessEngineConfiguration
+            .createStandaloneProcessEngineConfiguration();
+//    processEngineConfiguration.setJdbcDriver("com.mysql.jdbc.Driver");
+
+
+}
