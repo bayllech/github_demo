0355d7ef8bd0b2f29056c7217f2e972c88b4b752
bayllech <hpuqibei@163.com>
2017-03-13 16:50:01 +0800

    hibernate测试成功，并保存一个对象到数据库中

---
+++ hibernate/src/test/java/cn/bayllech/test/HibernateBasicTest.java
@@ -0,0 +1,32 @@
+package cn.bayllech.test;
+
+
+import cn.bayllech.pojo.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.junit.Test;
+
+public class HibernateBasicTest {
+    @Test
+    public void hibernateBuilder() {
+        Configuration configuration = new Configuration().configure();
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties())
+                .build();
+        SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+
+        Session session = sessionFactory.getCurrentSession();
+        session.getTransaction().begin();
+
+        User user = new User();
+        user.setUsername("hibernate");
+        user.setPassword("666");
+
+        session.save(user);
+
+        session.getTransaction().commit();
+    }
+}
