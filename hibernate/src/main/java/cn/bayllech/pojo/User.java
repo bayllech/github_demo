89bcf61c7cb554790456df79c8137b55f3749d71
bayllech <hpuqibei@163.com>
2017-03-13 16:26:31 +0800

    hibernate配置完成

---
+++ hibernate/src/main/java/cn/bayllech/pojo/User.java
@@ -0,0 +1,12 @@
+package cn.bayllech.pojo;
+
+import lombok.Data;
+
+@Data
+public class User {
+  private Integer id;
+  private String username;
+  private String password;
+  private String mobile;
+
+}
