d859ccd75dcbbcaa92095fd89b6fcc9df2cbee8a
bayllech <hpuqibei@163.com>
2017-03-14 11:08:51 +0800

    pojo的三种状态转换

--- hibernate/src/test/java/cn/bayllech/test/HibernateBasicTest.java
+++ hibernate/src/test/java/cn/bayllech/test/HibernateBasicTest.java
@@ -3,12 +3,16 @@ package cn.bayllech.test;
 
 import cn.bayllech.pojo.User;
 import cn.bayllech.util.HibernateUtil;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 import org.junit.Test;
+
+import javax.sound.midi.Soundbank;
+import java.util.List;
 
 public class HibernateBasicTest {
     @Test
@@ -37,6 +41,70 @@ public class HibernateBasicTest {
         session.beginTransaction();
         User user = (User) session.get(User.class, 1025);
         System.out.println(user);
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void updateUser() {
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+        User user = (User) session.get(User.class, 1025);
+        user.setMobile("123123231");
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void findAll() {
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+        Query query = session.createQuery("from User");
+        List<User> userList = query.list();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void getAndLoad() {
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+//        User user = (User) session.get(User.class, 1333);
+        User user = (User) session.load(User.class, 1034);
+//        System.out.println(user);
+
+        session.getTransaction().commit();
+    }
+
+    @Test
+    public void saveAndUpdate() {
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+        User user = (User) session.get(User.class, 1025);
+        session.getTransaction().commit();
+
+        Session session2 = HibernateUtil.getSession();
+        session2.beginTransaction();
+        user.setMobile("999");
+        session2.update(user);
+
+        session2.getTransaction().commit();
+    }
+
+    @Test
+    public void saveAndUpdateAndMerge() {
+        Session session = HibernateUtil.getSession();
+        session.beginTransaction();
+        User user = new User();
+        user.setId(1010);
+        user.setUsername("李四");
+
+        User user1 = (User) session.get(User.class, 1010);
+        System.out.println(user1);
+//        session.saveOrUpdate(user);
+        session.merge(user);
         session.getTransaction().commit();
     }
 }
