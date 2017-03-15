6094b871e66eff5de73b373486ecbdf88200b69c
bayllech <hpuqibei@163.com>
2017-03-15 15:06:48 +0800

    多对多，一方维护关系，延迟加载方案

---
+++ hibernate/src/main/java/cn/bayllech/pojo/Student.java
@@ -0,0 +1,33 @@
+package cn.bayllech.pojo;
+
+import java.util.Set;
+
+public class Student {
+    private Integer id;
+    private String sname;
+    private Set<Teacher> teacherSet;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public Set<Teacher> getTeacherSet() {
+        return teacherSet;
+    }
+
+    public void setTeacherSet(Set<Teacher> teacherSet) {
+        this.teacherSet = teacherSet;
+    }
+}
---
+++ hibernate/src/main/java/cn/bayllech/pojo/Teacher.java
@@ -0,0 +1,33 @@
+package cn.bayllech.pojo;
+
+import java.util.Set;
+
+public class Teacher {
+    private Integer id;
+    private String tname;
+    private Set<Student> studentSet;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTname() {
+        return tname;
+    }
+
+    public void setTname(String tname) {
+        this.tname = tname;
+    }
+
+    public Set<Student> getStudentSet() {
+        return studentSet;
+    }
+
+    public void setStudentSet(Set<Student> studentSet) {
+        this.studentSet = studentSet;
+    }
+}
