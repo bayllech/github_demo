94ef9e0a2e9365ab1b369b7777e4f86b2e3f93d2
bayllech <hpuqibei@163.com>
2017-03-20 21:20:10 +0800

    自定义拦截器完成

---
+++ struts2/src/main/java/cn/bayllech/action/LoginAction.java
@@ -0,0 +1,47 @@
+package cn.bayllech.action;
+
+public class LoginAction extends BaseAction {
+
+    private String userName;
+    private String password;
+    private String code;
+
+    @Override
+    public String execute() throws Exception {
+        return SUCCESS;
+    }
+
+    public String login() throws Exception {
+        if (userName.equals("tom") && password.equals("123")) {
+            getSession().put("current_user", "tom");
+            return SUCCESS;
+        } else {
+            code = "1002";
+            return ERROR;
+        }
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
